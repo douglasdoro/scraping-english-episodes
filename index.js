6ae9e00dc8cb4637ad5429ba66dd55309870632d
dorô <douglasdoro@gmail.com>
2020-06-14 21:32:52 -0300

    some adjustes to better performance

--- index.js
+++ index.js
@@ -2,55 +2,62 @@ const puppeteer = require('puppeteer');
 const wget = require('node-wget-promise');
 const path = require('path');
 const url = 'https://speakenglishpodcast.com/podcast/';
+let browser = null; 
 
 (async () => {
-  const defaultLinks = await loadLinksFrom(url); 
-  const totalPages = parseInt(defaultLinks[0].description.slice(1, 4)) / defaultLinks.length;   
+  browser = await puppeteer.launch({headless: false});
+  const episodesFromFirstPage = await loadLinksFrom(url); 
+  const numberOfEpisodes = discoverTotalEpisodes(episodesFromFirstPage); 
+  const numberOfPages = calculateTotalOfPages(numberOfEpisodes, episodesFromFirstPage.length); 
 
-  for(let i = 1; i <= totalPages; i ++) {
-      const links = await loadLinksFrom(url, i); 
+  for(let i = 1; i <= numberOfPages; i ++) {
+      const episodes =  i == 1 ? episodesFromFirstPage : await loadLinksFrom(url, i); 
 
       console.log('Page: ', i);
 
-      links.forEach(async link => {
-        const pdfLink = await getLinkPDFLink(link.url); 
-
-        downloadFifle(pdfLink, link.description); 
-        console.log('Donloading: ', link.description);
+      episodes.forEach(async episode => {
+        getLinkPDFLink(episode.url, episode.description); 
       });    
   }
 
-  console.log('Finished!');
+  setTimeout(async () => {
+    await browser.close();  
+    console.log('Finished!');
+  }, 10000);
+    
 })();
 
-async function loadLinksFrom(url, pageNumber = 1) {
-  const browser = await puppeteer.launch(); 
-  const page = await browser.newPage(); 
+function discoverTotalEpisodes(episodes) {
+  return parseInt(episodes[0].description.slice(1, 4));  
+}
 
+function calculateTotalOfPages(numberOfEpisodes, numberOfEpisodesPerPage) {
+  return numberOfEpisodes / numberOfEpisodesPerPage; 
+}
+
+async function loadLinksFrom(url, pageNumber = 1) {
+  const page = await browser.newPage(); 
   await page.goto(`${url}?_page=${pageNumber}`, { waitUntil: 'networkidle2'}); 
 
   const links = await page.evaluate(() => {
     return Array.from(document.querySelectorAll('p.pt-cv-title a._self')).map(item => { return { url: item.href, description: item.text }});
   });
 
-  await browser.close();  
+  page.close(); 
 
   return links; 
 }
 
-async function getLinkPDFLink(url) {
-  const browser = await puppeteer.launch(); 
+async function getLinkPDFLink(url, description) {
   const page = await browser.newPage(); 
-  //page.setDefaultTimeout(4000); 
   await page.goto(url, { waitUntil: 'networkidle2'}); 
-
-  const pdfLink = await page.evaluate(() => {
+  await page.evaluate(() => {
     return document.querySelectorAll('.et-boc ul li a')[1].href; 
+  }).then(pdfUrl => {
+    downloadFifle(pdfUrl, description); 
   });
 
-  await browser.close(); 
-
-  return pdfLink;
+  page.close(); 
 }
 
  function downloadFifle(url, filename) {
@@ -58,7 +65,11 @@ async function getLinkPDFLink(url) {
   {
     output: path.join(__dirname, `/pdf/${filename}.pdf`) 
   })
-  .then(metadata => metadata);
+  .then(metadata => {
+
+    console.log('Donloading: ', filename );
+    console.log('Size: ', metadata.fileSize );
+  });
 }
 
 
